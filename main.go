4313fd118ddd9d6bbcfb26fb6287b753f7d2b4bb
Eilidh Robey <eilidh.robey@coursehero.com>
2024-08-23 14:48:04 -0700

    output formatting

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ func main() {
 
 	fmt.Printf("Theme: %s\n", t.Name)
 	fmt.Printf("- Description: %s\n", t.Description)
-	for k := 0; k < 3; k++ {
-		fmt.Printf("- Feature: %s\n", t.GetFeatureString())
-		fmt.Printf("  - Danger: %s\n", t.GetDangerString())
-		fmt.Printf("  - Danger: %s\n", t.GetDangerString())
-	}
+	fmt.Printf("- Feature: %s\n", t.GetFeatureString())
+	fmt.Printf("  - Danger: %s\n", t.GetDangerString())
+	fmt.Println()
 }
